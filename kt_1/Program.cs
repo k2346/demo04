38297966aae3a631a1e30d5a27fe5b887ed9c726
k2346 <k2346@student.jamk.fi>
2016-02-03 16:24:50 +0200

    radio kt5 valmis

--- kt_1/Program.cs
+++ kt_1/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace kt_5
+namespace kt_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            //tehdään uusi radio ja asetetaan siihen arvoja
+            Radio radio = new Radio();
+            radio.OnOff = true;
+            radio.Frequency = 15000.0;
+            radio.Volume = 8;
+            Console.WriteLine("Radio is on: {0}", radio.OnOff);
+            Console.WriteLine("Radio Frequency is: " + radio.Frequency);
+            Console.WriteLine("Radio volume is: " + radio.Volume);
+            Console.WriteLine("");
+           
+            
+
+
+            //tehdään toinen radio ja asetetaan siihen arvoja
+            Radio radio1 = new Radio();
+            radio1.OnOff = true;
+            radio1.Frequency = 2155648415.0;
+            radio1.Volume = -2;
+            Console.WriteLine("Radio is on: {0}", radio1.OnOff);
+            Console.WriteLine("Radio Frequency is: " + radio1.Frequency);
+            Console.WriteLine("Radio volume is: " + radio1.Volume);
+            Console.ReadLine();
         }
     }
 }
---
+++ kt_1/Radio.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kt_1
+{
+    class Radio
+    {
+
+        //field variables //sisäiset ominaisuudet ei näy ulospäin, ei pysty muuttamaan
+        private readonly int maxVolume = 10;
+       private readonly int minVolume = 0;
+        private readonly double maxFrequency = 26000.0;
+        private readonly double minFrequency = 2000.0;
+
+        private int volume; //tämä aina pienellä
+        private double frequency;
+
+
+        //property variable // näitä arvoja käyttäjä pystyy muuttamaan jos, eivät ylitä minimi tai maksimi arvoja.
+        public int Volume
+        {
+            get
+            {
+                return volume; //pienellä
+            }
+
+            set
+            {
+                if (value < 0) //onko pienempi kuin minimi volume
+                {
+                    Console.WriteLine("Too silent!! - Set to min");
+                    volume = minVolume;
+                }
+                else if (value > maxVolume)
+                {
+                    Console.WriteLine("Too loud!! - Set to max");
+                    volume = maxVolume;
+                }
+                else
+                {
+                    volume = value;
+                }
+            }
+
+        }
+
+
+        public double Frequency
+        {
+            get
+            {
+                return frequency; //pienellä
+            }
+
+            set
+            {
+                if (value < 2000.0) //onko pienempi kuin minimi arvo
+                {
+                    Console.WriteLine("Not a channel - Set to min");
+                    frequency = minFrequency;
+                }
+                else if (value > maxFrequency)
+                {
+                    Console.WriteLine("Not a channel - Set to max");
+                    frequency = maxFrequency;
+                }
+                else
+                {
+                    frequency = value;
+                }
+            }
+
+        }
+
+
+        public bool OnOff { get; set; }
+
+
+
+    
+  }
+}
