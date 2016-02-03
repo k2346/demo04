2b7120df2081cd117d5cd87c3a8e5ae2384e868a
k2346 <k2346@student.jamk.fi>
2016-02-03 11:13:42 +0200

    Lisättiin hissi

---
+++ hissi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hissi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Hissi dynamoHissi = new Hissi
+        }
+    }
+}
